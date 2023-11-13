cd0c4cf176922d16ea8cd4ebc163d0ca322cb356
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2023-11-13 21:46:48 +0900

    bot: send today's news when joined

--- bot.py
+++ bot.py
@@ -9,30 +9,44 @@ import getNews
 
 load_dotenv(verbose = True)
 
+async def sendEmbeds(newsArray, ctx):
+    if not ctx:
+        embeds = []
+
+        # if the newsArray is not empty // there is/are new news
+        for news in newsArray:
+            embed = discord.Embed(title = news[0], url = news[2], description = "새로운 포켓몬 뉴스가 도착했습니다!", color = discord.Color.from_rgb(231, 0, 9))
+            embed.set_thumbnail(url = news[1])
+            embeds.append(embed)
+
+        channel = bot.get_channel(1160171194344034374)
+        await channel.send(embeds = embeds)
+        embeds = []
+    else:
+        embeds = []
+
+        for news in newsArray:
+            embed = discord.Embed(title = news[0], url = news[2], description = f"{str(newsArray.index(news) + 1)}번째 오래된 뉴스", color = discord.Color.from_rgb(231, 0, 9))
+            embed.set_thumbnail(url = news[1])
+            embed.add_field(name = "업데이트 날짜:", value = news[3], inline = False)
+            embed.add_field(name = "모든 뉴스 확인:", value = "[포켓몬SV 뉴스](https://sv-news.pokemon.co.jp/ko/list)", inline = False)
+            embeds.append(embed)
+
+        await ctx.response.send_message(embeds = embeds)
+        embeds = []
+
 # the function that runs the bot
 def run_discord_bot():
     TOKEN = os.getenv("TOKEN")
-    intents = discord.Intents.default()
-    intents.message_content = True
-    # use '.' before command in discord for this bot to respond
-    bot = commands.Bot(command_prefix = ".", intents = intents)
 
     # iterating twice a day to get recent news
     @tasks.loop(hours = 24)
     async def scrape():
         newsArray = getNews.getTodayNews()
-        embeds = []
 
         # if the newsArray is not empty // there is/are new news
         if newsArray:
-            for news in newsArray:
-                embed = discord.Embed(title = news[0], url = news[2], description = "새로운 포켓몬 뉴스가 도착했습니다!", color = discord.Color.from_rgb(231, 0, 9))
-                embed.set_thumbnail(url = news[1])
-                embeds.append(embed)
-
-            channel = bot.get_channel(1160171194344034374)
-            await channel.send(embeds = embeds)
-            embeds = []
+            sendEmbeds(newsArray)
 
     @scrape.before_loop
     async def scrape_before():
@@ -53,6 +67,15 @@ def run_discord_bot():
         except Exception as e:
             print(e)
 
+    @bot.event
+    async def on_guild_join(guild):
+        print(f"Bot is added to: {guild}")
+
+        newsArray = getNews.getTodayNews()
+
+        if newsArray:
+            sendEmbeds(newsArray)
+
     # /help를 친 사람에게만 보이도록 ephemeral = True
     @bot.tree.command(name = "help")
     async def help(ctx: discord.Interaction):
@@ -61,15 +84,6 @@ def run_discord_bot():
     @bot.tree.command(name = "prevnews")
     async def prevNews(ctx: discord.Interaction):
         newsArray = getNews.getPrevNews()
-        embeds = []
-
-        for news in newsArray:
-            embed = discord.Embed(title = news[0], url = news[2], description = f"{str(newsArray.index(news) + 1)}번째 오래된 뉴스", color = discord.Color.from_rgb(231, 0, 9))
-            embed.set_thumbnail(url = news[1])
-            embed.add_field(name = "업데이트 날짜:", value = news[3], inline = False)
-            embeds.append(embed)
-
-        await ctx.response.send_message(embeds = embeds)
-        embeds = []
+        sendEmbeds(newsArray, ctx)
 
     bot.run(TOKEN)
